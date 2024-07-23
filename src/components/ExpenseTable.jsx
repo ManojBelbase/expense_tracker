8afac25a1d310bc4420b04d6e68ea84a0876e667
Manoj Belbase <manojbelbase56@gmail.com>
2024-07-23 19:11:48 +0545

    sorting amount and title in ascending and desending order

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -16,6 +16,7 @@ const ExpenseTable = ({
     (accu, curr) => accu + parseInt(curr.amount),
     0
   );
+  console.log("render");
   return (
     <>
       <Contextmenu
@@ -27,10 +28,53 @@ const ExpenseTable = ({
         setExpense={setExpense}
         seteditingRowId={seteditingRowId}
       />
-      <table className="expense-table" onClick={() => setMenuPosition({})}>
+      <table
+        className="expense-table"
+        onClick={() => {
+          if (menuPosition.left) {
+            setMenuPosition({});
+          }
+        }}
+      >
         <thead>
           <tr>
-            <th>Title</th>
+            <th>
+              <div>
+                <span>Title</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={10}
+                  viewBox="0 0 384 512"
+                  className="arrow up-arrow"
+                  onClick={() =>
+                    setExpenses((prevState) => [
+                      ...prevState.sort((a, b) =>
+                        b.title.localeCompare(a.title)
+                      ),
+                    ])
+                  }
+                >
+                  <title>Ascending</title>
+                  <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={10}
+                  viewBox="0 0 384 512"
+                  className="arrow down-arrow"
+                  onClick={() =>
+                    setExpenses((prevState) => [
+                      ...prevState.sort((a, b) =>
+                        a.title.localeCompare(b.title)
+                      ),
+                    ])
+                  }
+                >
+                  <title>Descending</title>
+                  <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                </svg>
+              </div>
+            </th>
             <th>
               <select onChange={(e) => setQuery(e.target.value.toLowerCase())}>
                 <option value="">All</option>
@@ -49,6 +93,11 @@ const ExpenseTable = ({
                   width={10}
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
+                  onClick={() =>
+                    setExpenses((prevState) => [
+                      ...prevState.sort((a, b) => b.amount - a.amount),
+                    ])
+                  }
                 >
                   <title>Ascending</title>
                   <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
@@ -58,6 +107,11 @@ const ExpenseTable = ({
                   width={10}
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
+                  onClick={() => {
+                    setExpenses((prevState) => [
+                      ...prevState.sort((a, b) => a.amount - b.amount),
+                    ]);
+                  }}
                 >
                   <title>Descending</title>
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
